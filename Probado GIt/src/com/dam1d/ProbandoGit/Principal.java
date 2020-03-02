e77a26cdf5b1a5bf420ebb83cad0b1326c417d08
Yoan F <dam.jfogued.195@iespabloserrano.com>
2020-03-02 10:56:55 +0100

    Intenta matar la Tusa

--- Probado GIt/src/com/dam1d/ProbandoGit/Principal.java
+++ Probado GIt/src/com/dam1d/ProbandoGit/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 	System.out.println("hola");
 	for(int i=1; i>0; i++) {
-		System.out.println(Otra.mostrar());
+		System.out.print(Otra.mostrar() + "\t" + Otra.mostrar() + "\t" + Otra.mostrar() + "\t");
 	}
 	}
 
