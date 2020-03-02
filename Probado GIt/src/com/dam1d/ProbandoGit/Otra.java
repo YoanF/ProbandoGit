452fc9f1af59ee01ec3f8a17f83cc232dbd4a0c3
Yoan F <dam.jfogued.195@iespabloserrano.com>
2020-03-02 10:45:56 +0100

    Segundo commit

---
+++ Probado GIt/src/com/dam1d/ProbandoGit/Otra.java
@@ -0,0 +1,9 @@
+package com.dam1d.ProbandoGit;
+
+public class Otra {
+	
+	void mostrar() {
+		System.out.println("pon Tusa");
+	}
+	
+}
--- Probado GIt/src/com/dam1d/ProbandoGit/Principal.java
+++ Probado GIt/src/com/dam1d/ProbandoGit/Principal.java
@@ -3,8 +3,7 @@ package com.dam1d.ProbandoGit;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	System.out.println("hola");
 	}
 
 }
